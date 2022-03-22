5fd5f76b0cf1455b542725087349693bb1be8c33
simonjisu <simonjisu@gmail.com>
2022-03-22 15:00:22 +0900

    update

--- build-book.sh
+++ build-book.sh
@@ -3,7 +3,7 @@ git add .
 git commit -m 'update'
 git push origin main
 git checkout gh-pages
-git pull origin main
+git merge origin main
 rm -rf docs
 jupyter-book build book --path-output ./
 mv _build docs
