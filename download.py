04efbc9ca8e8ec7c1a48b97214c78b2e073ac226
simonjisu <simonjisu@gmail.com>
2022-03-22 15:11:31 +0900

    Update documentation

--- download.py
+++
@@ -1,8 +0,0 @@
-import gdown
-from pathlib import Path
-url = 'https://drive.google.com/uc?id=1ppI6B488QgYSC6pXI2z7TVDdMYPGRhyx'
-data_path = Path() / 'data'
-if not data_path.exists():
-    data_path.mkdir()
-output = data_path / 'data.zip'
-gdown.download(url, output, quiet=False)
