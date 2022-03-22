a4944e516ab5b82b973929d1e184f6233bc9a2fb
simonjisu <simonjisu@gmail.com>
2022-03-22 13:01:11 +0900

    update

---
+++ build-book.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git checkout gh-pages
+git pull origin main
+jupyter-book build book --path-output ./docs
+git add .
+git commit -m 'update book'
+git push origin gh-pages
+git checkout main
